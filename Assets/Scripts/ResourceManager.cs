8f23b38e0f4b0a6ad82f6d29fd283df24163c7e9
Iliketoshootunity <34265768+Iliketoshootunity@users.noreply.github.com>
2019-10-15 18:01:26 +0800

    Update ResourceManager.cs

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -39,7 +39,11 @@ public class DoubleLinkedList<T> where T : class, new()
             return m_Count;
         }
     }
-
+    /// <summary>
+    ///  添加到表头
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
     public DoubleLinkedListNode<T> AddToHead(T t)
     {
         if (t == null)
@@ -52,7 +56,11 @@ public class DoubleLinkedList<T> where T : class, new()
         temp.Content = t;
         return AddToHead(temp);
     }
-
+    /// <summary>
+    /// 添加到表头
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
     public DoubleLinkedListNode<T> AddToHead(DoubleLinkedListNode<T> node)
     {
         if (node == null)
@@ -67,9 +75,111 @@ public class DoubleLinkedList<T> where T : class, new()
         {
             node.Prev = null;
             Head.Prev = node;
-            node = Head;
+            node.Next = Head;
+            Head = node;
 
         }
+        m_Count++;
         return node;
     }
+    /// <summary>
+    /// 添加到表尾
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToTail(T t)
+    {
+        if (t == null)
+        {
+            return null;
+        }
+        DoubleLinkedListNode<T> temp = NodePool.Spawn(true);
+        temp.Next = null;
+        temp.Prev = null;
+        temp.Content = t;
+        return AddToTail(temp);
+    }
+
+    /// <summary>
+    /// 添加到表尾
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToTail(DoubleLinkedListNode<T> node)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+        node.Next = null;
+        Tail.Next = node;
+        node.Prev = Tail;
+        Tail = node;
+        return Tail;
+    }
+    /// <summary>
+    /// 移除节点
+    /// </summary>
+    /// <param name="node"></param>
+    public void RemoveNode(DoubleLinkedListNode<T> node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        if (node == Head)
+        {
+            Head = node.Next;
+        }
+        if (node == Tail)
+        {
+            Tail = node.Prev;
+        }
+        if (node.Prev != null)
+            node.Prev.Next = node.Next;
+        if (node.Next != null)
+            node.Next.Prev = node.Prev;
+        node.Prev = null;
+        node.Next = null;
+        NodePool.Recycle(node);
+        m_Count--;
+    }
+    /// <summary>
+    /// 移动到头部
+    /// </summary>
+    /// <param name="node"></param>
+    public void MoveToHead(DoubleLinkedListNode<T> node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        if (node == Head)
+        {
+            return;
+        }
+        if (node.Prev == null && node.Next == null) return;
+        if (node == Tail)
+        {
+            Tail = node.Prev;
+        }
+        if (node.Prev != null)
+            node.Prev.Next = node.Next;
+        if (node.Next != null)
+            node.Next.Prev = node.Prev;
+
+        node.Prev = null;
+        node.Next = Head;
+        Head.Prev = node;
+
+        if (Tail == null)
+        {
+            Tail = Head;
+        }
+    }
+}
+
+public class CMapList<T> where T : class, new()
+{
+
 }
